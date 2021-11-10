7e05736e97081d1440a713a52f319ae0e3f0c7b2
MariePetit572 <93980106+MariePetit572@users.noreply.github.com>
2021-11-10 15:18:11 +0100

    fin ex5

---
+++ ex5/assets/js/script.js
@@ -0,0 +1,7 @@
+var bouton=document.getElementById('button');
+bouton.addEventListener('click', function button(){
+var nbr1 = document.getElementById("nbr1").value;
+var nbr2 = document.getElementById("nbr2").value;
+
+alert ( nbr1 % nbr2);
+})
