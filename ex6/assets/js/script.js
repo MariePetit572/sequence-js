c9cb7fc20f78b2403b60c536063b8ba121a216b7
MariePetit572 <93980106+MariePetit572@users.noreply.github.com>
2021-11-10 15:38:15 +0100

    fin ex6

---
+++ ex6/assets/js/script.js
@@ -0,0 +1,7 @@
+var button=document.getElementById('button');
+button.addEventListener('click', function button(){
+var pointure = document.getElementById("pointure").value;
+var annee = document.getElementById("annee").value;
+
+alert ( ((((pointure*2)+5)*50)-annee)+1769);
+})
