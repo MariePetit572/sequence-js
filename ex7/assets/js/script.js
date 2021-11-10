52875bfb873e0bce321754c5b5813a07e33d13a0
MariePetit572 <93980106+MariePetit572@users.noreply.github.com>
2021-11-10 16:25:55 +0100

    fin ex7

---
+++ ex7/assets/js/script.js
@@ -0,0 +1,6 @@
+var button=document.getElementById('button');
+button.addEventListener('click', function button(){
+var age = document.getElementById('age').value;
+if(age>=18){ alert ('Vous êtes majeur');}
+else {alert('vous êtes mineur');} 
+})
