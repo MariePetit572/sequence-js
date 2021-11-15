cb54037a30223d5987fbef33db94cc87474d1134
MariePetit572 <93980106+MariePetit572@users.noreply.github.com>
2021-11-15 10:39:42 +0100

    correction

--- ex6/assets/js/script.js
+++ ex6/assets/js/script.js
@@ -3,5 +3,5 @@ button.addEventListener('click', function button(){
 var pointure = document.getElementById("pointure").value;
 var annee = document.getElementById("annee").value;
 
-alert ( ((((pointure*2)+5)*50)-annee)+1769);
+alert ( ((((pointure*2)+5)*50)-annee)+1771);
 })
