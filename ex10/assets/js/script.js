e12691d618dee83df1954ed187d430925ed7104a
MariePetit572 <93980106+MariePetit572@users.noreply.github.com>
2021-11-15 10:39:13 +0100

    fin ex10

---
+++ ex10/assets/js/script.js
@@ -0,0 +1,12 @@
+var prenom='Marie';
+var userName=document.getElementById('userName');
+
+userName.addEventListener('keyup', function () {
+    if (userName.value!=prenom) {
+         userName.style.border= 'red 6px solid';
+    blocAlert.innerHTML='<div class=" alert alert-danger">Mauvaise réponse!</div>';
+}  else { 
+        userName.style.border= 'green 6px solid';
+    blocAlert.innerHTML='<div class=" alert alert-success">Bonne réponse!</div>';}
+ })
+
