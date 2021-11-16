4af7cf4fb23ed34450fe0bef7b90b6af5a75953e
MariePetit572 <93980106+MariePetit572@users.noreply.github.com>
2021-11-16 18:08:44 +0100

    correction ex 9

--- ex9/assets/js/script.js
+++ ex9/assets/js/script.js
@@ -1,6 +1,13 @@
+var initwidthimg =img.clientWidth;
 var img=document.getElementById('img');
-img.onclick=function(){
-    {
-    img.style.width='700px'
-     }
-}
+// verifier la taille de l image initiale en f12:
+// console.log(img.clientWidth);
+img.addEventListener('click',function(){
+    var widthimg =img.clientWidth;
+    if(widthimg>1500){
+        img.style.width=(initwidthimg+'px')
+    } else { 
+        img.style.width= (widthimg*1.1+'px')
+ }
+});
+// correction https://github.com/Manito764/Exercices-JS
